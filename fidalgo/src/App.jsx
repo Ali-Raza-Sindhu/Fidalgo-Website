e2957178b85140fe1c9b6a58c9803f52047b9a1b
AliRazaSindhu <ssar6818@gmail.com>
2026-01-04 22:12:24 +0500

    initial

--- fidalgo/src/App.jsx
+++ fidalgo/src/App.jsx
@@ -1,16 +1,16 @@
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import Password from "./pages/Password.jsx"
 
 function App() {
 
   return (
     <>
     <BrowserRouter>
-    
-      {/* <Navbar/> */}
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/password" element={<Password/>} />
       </Routes>
       
     </BrowserRouter>
